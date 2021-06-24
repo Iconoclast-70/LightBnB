12d2a99b44925c2130c1a7a4efe7d899e2231a6e
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-24 04:50:09 +0000

    lightbnb web boilerplate

--- 1_queries/most_visited_cities.sql
+++ 1_queries/most_visited_cities.sql
@@ -1,12 +1,5 @@
-SELECT properties.city, COUNT(reservations)
-AS total_reservations
+SELECT DISTINCT properties.city, COUNT(reservations) AS total_reservations
 FROM properties 
 JOIN reservations ON reservations.property_id = properties.id
-GROUP BY properties.id
+GROUP BY properties.city
 ORDER BY total_reservations DESC;
-
-
-
-
-Select the name of the city and the number of reservations for that city.
-Order the results from highest number of reservations to lowest number of reservations.
